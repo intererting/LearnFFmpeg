61c6b76ef2da32d1c5d4ee42de2e4e6e4b84c363
645644717@qq.com <198616yuy>
2019-12-13 10:01:47 +0800

    添加yuv 转 h264

--- code/yuv_to_h264.cpp
+++ code/yuv_to_h264.cpp
@@ -6,59 +6,29 @@ extern "C"
 #include <libavutil/opt.h>
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
+#include "libavutil/imgutils.h"
 };
-using namespace std;
-
-int flush_encoder(AVFormatContext *fmt_ctx, unsigned int stream_index) {
-    int ret;
-    int got_frame;
-    AVPacket enc_pkt;
-    while (true) {
-        enc_pkt.data = NULL;
-        enc_pkt.size = 0;
-        av_init_packet(&enc_pkt);
-        ret = avcodec_encode_video2(fmt_ctx->streams[stream_index]->codec, &enc_pkt,
-                                    NULL, &got_frame);
-        av_frame_free(NULL);
-        if (ret < 0)
-            break;
-        if (!got_frame) {
-            ret = 0;
-            break;
-        }
-        printf("Flush Encoder: Succeed to encode 1 frame!\tsize:%5d\n", enc_pkt.size);
-        /* mux encoded frame */
-        ret = av_write_frame(fmt_ctx, &enc_pkt);
-        if (ret < 0)
-            break;
-    }
-    return ret;
-}
 
 int main() {
-
     AVFormatContext *pFormatCtx;
     AVOutputFormat *fmt;
     AVStream *video_st;
-    AVCodecContext *pCodecCtx;
+    AVCodecContext *pCodecCtx = avcodec_alloc_context3(avcodec_find_encoder(AV_CODEC_ID_H265));;
     AVCodec *pCodec;
     AVPacket pkt;
     uint8_t *picture_buf;
     AVFrame *pFrame;
     int picture_size;
     int y_size;
-    int framecnt = 0;
-
     //获取yuv文件
     FILE *in_file = fopen(R"(C:\Users\user\Desktop\LearnFFmpeg\ds_480x272.yuv)", "rb");
     int in_w = 480, in_h = 272;                              //Input data's width and height
     int framenum = 100;                                   //Frames to encode
     //const char* out_file = "src01.h264";              //Output Filepath
     //const char* out_file = "src01.ts";
-    //const char* out_file = "src01.hevc";
-    const char *out_file = R"(C:\Users\user\Desktop\LearnFFmpeg\ds.264)";
+//    const char* out_file = "ds.hevc";
+    const char *out_file = R"(C:\Users\user\Desktop\LearnFFmpeg\ds.hevc)";
 
-    //Method1.
     pFormatCtx = avformat_alloc_context();
     //Guess Format
     fmt = av_guess_format(nullptr, out_file, nullptr);
@@ -66,43 +36,34 @@ int main() {
     pFormatCtx->oformat = fmt;
 
     //Open output URL
-    if (avio_open(&pFormatCtx->pb, out_file, AVIO_FLAG_READ_WRITE) < 0) {
+    if (avio_open(&pFormatCtx->pb, out_file, AVIO_FLAG_WRITE) < 0) {
         printf("Failed to open output file! \n");
         return -1;
     }
-
     video_st = avformat_new_stream(pFormatCtx, nullptr);
-//    video_st->time_base.num = 1;
-//    video_st->time_base.den = 25;
-
     if (nullptr == video_st) {
         return -1;
     }
-
-    //Param that must set
-    pCodecCtx = video_st->codec;
-    //pCodecCtx->codec_id =AV_CODEC_ID_HEVC;
-    pCodecCtx->codec_id = fmt->video_codec;
-    pCodecCtx->codec_type = AVMEDIA_TYPE_VIDEO;
+    //这三个参数必须设置
+    video_st->codecpar->codec_type = AVMEDIA_TYPE_VIDEO;
+    video_st->codecpar->width = in_w;
+    video_st->codecpar->height = in_h;
+    pCodecCtx->codec_type = video_st->codecpar->codec_type;
     pCodecCtx->pix_fmt = AV_PIX_FMT_YUV420P;
-    pCodecCtx->width = in_w;
-    pCodecCtx->height = in_h;
+    pCodecCtx->width = video_st->codecpar->width;
+    pCodecCtx->height = video_st->codecpar->height;
     pCodecCtx->bit_rate = 400000;
     pCodecCtx->gop_size = 250;
-
     pCodecCtx->time_base.num = 1;
     pCodecCtx->time_base.den = 25;
-
     //H264
     //pCodecCtx->me_range = 16;
     //pCodecCtx->max_qdiff = 4;
     //pCodecCtx->qcompress = 0.6;
     pCodecCtx->qmin = 10;
     pCodecCtx->qmax = 51;
-
     //Optional Param
     pCodecCtx->max_b_frames = 3;
-
     // Set Option
     AVDictionary *param = nullptr;
     //H.264
@@ -116,10 +77,8 @@ int main() {
         av_dict_set(&param, "preset", "ultrafast", 0);
         av_dict_set(&param, "tune", "zero-latency", 0);
     }
-
     //Show some Information
     av_dump_format(pFormatCtx, 0, out_file, 1);
-
     pCodec = avcodec_find_encoder(pCodecCtx->codec_id);
     if (!pCodec) {
         printf("Can not find encoder! \n");
@@ -129,16 +88,13 @@ int main() {
         printf("Failed to open encoder! \n");
         return -1;
     }
-
-
     pFrame = av_frame_alloc();
-    picture_size = avpicture_get_size(pCodecCtx->pix_fmt, pCodecCtx->width, pCodecCtx->height);
+    picture_size = av_image_get_buffer_size(AV_PIX_FMT_YUV420P, pCodecCtx->width, pCodecCtx->height, 1);
     picture_buf = (uint8_t *) av_malloc(picture_size);
-    avpicture_fill((AVPicture *) pFrame, picture_buf, pCodecCtx->pix_fmt, pCodecCtx->width, pCodecCtx->height);
-
+    av_image_fill_arrays(pFrame->data, pFrame->linesize, picture_buf, pCodecCtx->pix_fmt, pCodecCtx->width,
+                         pCodecCtx->height, 1);
     //Write File Header
-    avformat_write_header(pFormatCtx, NULL);
-
+    avformat_write_header(pFormatCtx, nullptr);
     av_new_packet(&pkt, picture_size);
 
     y_size = pCodecCtx->width * pCodecCtx->height;
@@ -156,44 +112,25 @@ int main() {
         pFrame->data[2] = picture_buf + y_size * 5 / 4;  // V
         //PTS
         //pFrame->pts=i;
+        //25 位fps
         pFrame->pts = i * (video_st->time_base.den) / ((video_st->time_base.num) * 25);
-        int got_picture = 0;
         //Encode
-        int ret = avcodec_encode_video2(pCodecCtx, &pkt, pFrame, &got_picture);
-        if (ret < 0) {
-            printf("Failed to encode! \n");
-            return -1;
-        }
-        if (got_picture == 1) {
-            printf("Succeed to encode frame: %5d\tsize:%5d\n", framecnt, pkt.size);
-            framecnt++;
-            pkt.stream_index = video_st->index;
-            ret = av_write_frame(pFormatCtx, &pkt);
-            av_free_packet(&pkt);
-        }
+        avcodec_send_frame(pCodecCtx, pFrame);
+        avcodec_receive_packet(pCodecCtx, &pkt);
+        pkt.stream_index = video_st->index;
+        //写数据
+        av_write_frame(pFormatCtx, &pkt);
+        av_packet_unref(&pkt);
     }
-    //Flush Encoder
-    int ret = flush_encoder(pFormatCtx, 0);
-    if (ret < 0) {
-        printf("Flushing encoder failed\n");
-        return -1;
-    }
-
     //Write file trailer
     av_write_trailer(pFormatCtx);
-
     //Clean
-    if (video_st) {
-        avcodec_close(video_st->codec);
-        av_free(pFrame);
-        av_free(picture_buf);
-    }
+    avcodec_free_context(&pCodecCtx);
+    av_free(pFrame);
+    av_free(picture_buf);
     avio_close(pFormatCtx->pb);
     avformat_free_context(pFormatCtx);
-
     fclose(in_file);
-
-
     return 0;
 }
 
